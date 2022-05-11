5dbe3b03bc791582a05f2e3005161ea2eb3a3f11
Luyao Han <luyaohan1001@gmail.com>
2022-05-11 14:20:06 -0400

    added macro-functions

--- macro-functions.c
+++ macro-functions.c
@@ -1,3 +1,13 @@
+/**
+  ******** ******** ******** ******** ******** ******** ******** ******** ******** ******** ******** ******** ******** ******** ******** ********
+  * @file      :     macro-functions.c
+  * @author    :     Luyao Han
+  * @email     :     luyaohan1001@gmail.com
+  * @brief     :     A curated list of macro functions for reference.
+  * @date      :     05-11-2022
+  * Copyright (C) 2022-2122 Luyao Han. The following code may be shared or modified for personal use / non-commercial use only.
+  ******** ******** ******** ******** ******** ******** ******** ******** ******** ******** ******** ******** ******** ******** ******** ********  */
+
 #define lobyte(x)  ((uint8_t)((x) & 0x00ffu))
 
 #define hibyte(x)  ((uint8_t)(((x) & 0xff00u) >> 8u))
