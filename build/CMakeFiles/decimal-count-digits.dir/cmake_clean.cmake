1bf33563f0e8e60153c8b6a6e9d566ea76e7b368
Luyao Han <luyaohan1001@gmail.com>
2022-05-28 10:44:29 -0400

    updated decimal-count-digits.c

---
+++ build/CMakeFiles/decimal-count-digits.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/decimal-count-digits.dir/decimal-count-digits.c.o"
+  "CMakeFiles/decimal-count-digits.dir/decimal-count-digits.c.o.d"
+  "decimal-count-digits"
+  "decimal-count-digits.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/decimal-count-digits.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/decimal-count-digits.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for decimal-count-digits.
+# This may be replaced when dependencies are built.
